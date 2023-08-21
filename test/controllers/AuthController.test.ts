31bb1cf1870cad937159d93c1d682979d1a00454
Diego Cardenas <diegocardwr@gmail.com>
2023-08-21 16:06:55 -0500

    test: Add tests for login functionality

---
+++ test/controllers/AuthController.test.ts
@@ -0,0 +1,72 @@
+import request from 'supertest'
+import { User } from '../../src/interfaces/User';
+import { GlobalResponse } from "../../src/middlewares/ResponseMiddleware"
+import app from "../../src/index"
+import bc from "bcrypt";
+
+afterAll((done) => {
+    app.close();
+    done();
+});
+
+describe("POST /sesion", () => {
+
+    test("GOD DATA", async () => {
+        const body = {
+            email: "god@gmail.com",
+            password: "elPepe123@"
+        }
+
+        const expectedResponse:GlobalResponse<{User: User, token: string}> = {
+            errors: [],
+            success: true,
+            value: {
+                User: {} as User,
+                token: expect.any(String),
+            }
+        }
+
+        const response = await request(app.app).get("/sesion").send();
+        expect(response.body).toEqual(expectedResponse);
+        expect(response.statusCode).toBe(200);
+    });
+
+    test("WRONG EMAIL", async () => {
+        const body = {
+            email: "wrongEmail@gmail.com",
+            password: "elPepe123@"
+        }
+
+        const expectedResponse:GlobalResponse<any> = {
+            errors: [
+                "Correo no encontrado."
+            ],
+            success: false,
+            value: null
+        }
+
+        const response = await request(app.app).get("/sesion").send(body);
+        expect(response.body).toEqual(expectedResponse);
+        expect(response.statusCode).toBe(401);
+    });
+
+    test("WRONG PASSWORD", async () => {
+        const body = {
+            email: "god@gmail.com",
+            password: "wrongPassword"
+        }
+
+        const expectedResponse:GlobalResponse<any> = {
+            errors: [
+                "Contraseña incorrecta."
+            ],
+            success: false,
+            value: null
+        }
+
+        const response = await request(app.app).get("/sesion").send(body);
+        expect(response.body).toEqual(expectedResponse);
+        expect(response.statusCode).toBe(401);
+    });
+
+});

83c759aff3249777c2e3cd3688a212f78b7c20b3
Diego-Arciniegas <62758837+Diego-Arciniegas@users.noreply.github.com>
2023-08-25 22:01:28 -0500

    test: Add acceptance/rejection tests file for service

---
+++ test/controllers/ServiceStatus.test.ts
@@ -0,0 +1,76 @@
+import request from 'supertest'
+import { User } from '../../src/interfaces/User';
+import { GlobalResponse } from "../../src/middlewares/ResponseMiddleware"
+import app from "../../src/index"
+import bc from "bcrypt";
+import jwt from "jsonwebtoken";
+
+
+afterAll((done) => {
+    app.close();
+    done();
+});
+
+
+describe('PATCH /service-request/:id', () => {
+    test('Accept Request', async () => {
+        const requestId = 'Aun no hay ID en la base de datos'; 
+        const action = 'accept'; 
+
+        const expectedResponse = {
+            value: null,
+            errors: [],
+            success: true,
+        };
+
+        const response = await request(app.app).patch(`/service-request/${requestId}`).send({ action });
+        expect(response.body).toEqual(expectedResponse);
+        expect(response.statusCode).toBe(200);
+    });
+
+    test('Reject Request', async () => {
+        const requestId = 'Aun no hay ID en la base de datos'; 
+        const action = 'reject'; 
+
+        const expectedResponse = {
+            value: null,
+            errors: [],
+            success: true,
+        };
+
+        const response = await request(app.app).patch(`/service-request/${requestId}`).send({ action });
+        expect(response.body).toEqual(expectedResponse);
+        expect(response.statusCode).toBe(200);
+    });
+
+    test('Request Not Found', async () => {
+        const requestId = 'Aun no hay ID en la base de datos';
+        const action = 'accept';
+
+        const expectedResponse = {
+            value: null,
+            errors: ['Solicitud no encontrada.'],
+            success: false,
+        };
+
+        const response = await request(app.app).patch(`/service-request/${requestId}`).send({ action });
+        expect(response.body).toEqual(expectedResponse);
+        expect(response.statusCode).toBe(404);
+    });
+
+    test('Invalid Action', async () => {
+        const requestId = 'Aun no hay ID en la base de datos';
+        const invalidAction = 'invalidAction'; 
+
+        const expectedResponse = {
+            value: null,
+            errors: ['Acción inválida.'],
+            success: false,
+        };
+
+        const response = await request(app.app).patch(`/service-request/${requestId}`).send({ action: invalidAction });
+        expect(response.body).toEqual(expectedResponse);
+        expect(response.statusCode).toBe(400);
+    });
+
+});

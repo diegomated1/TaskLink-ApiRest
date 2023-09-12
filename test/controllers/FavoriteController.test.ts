b124f31340ab2a1eaf0e65549d291b4fcf98179b
Diego Cardenas <diegocardwr@gmail.com>
2023-09-12 18:31:15 -0500

    test: add test for post and delete favorites

--- test/controllers/FavoriteController.test.ts
+++ test/controllers/FavoriteController.test.ts
@@ -1,19 +1,18 @@
 import request from 'supertest'
-import { User } from '../../src/interfaces/User';
 import { GlobalResponse } from "../../src/middlewares/ResponseMiddleware"
 import app from "../../src/index"
-import bc from "bcrypt";
 
+const API_TOKEN_DEV = `Bearer ${process.env.API_TOKEN_DEV!}`;
 
 afterAll((done) => {
     app.close();
     done();
 });
 
-describe.skip("GET /favorites", () => {
+describe("GET /favorites", () => {
 
     test("GET ALL DATA", async () => {
-        const response = await request(app.app).get("/favorites").send();
+        const response = await request(app.app).get("/favorite").set("Authorization", API_TOKEN_DEV).send();
 
         expect(response.body).toHaveProperty("value");
         expect(response.body).toHaveProperty("errors");
@@ -24,6 +23,98 @@ describe.skip("GET /favorites", () => {
         expect(Array.isArray(response.body.value)).toBe(true);
 
         expect(response.statusCode).toBe(200);
+    });
+
+});
+
+describe("POST /favorites", () => {
+
+    test("GOOD", async () => {
+        const body = {
+            service_provider_id: "1dde026b-8b82-49b9-a9ed-1ed2d7208e86"
+        }
+
+        // REMOVE FAVORITE IF EXIXITS
+        await request(app.app).delete("/favorite").set("Authorization", API_TOKEN_DEV).send(body);
+
+        const expectedResponse:GlobalResponse<any> = {
+            errors: [],
+            success: true,
+            value: {
+                id: expect.any(Number),
+                user_id: "97f99c61-a665-4eb5-9dd1-799fd82ffd34",
+                service_provider_id: "1dde026b-8b82-49b9-a9ed-1ed2d7208e86"
+            }
+        }
+        const response = await request(app.app).post("/favorite").set("Authorization", API_TOKEN_DEV).send(body);
+
+        expect(response.body).toEqual(expectedResponse);
+        expect(response.statusCode).toBe(200);
+    });
+
+    test("NOT FOUND", async () => {
+
+        const body = {
+            service_provider_id: "1dde026b-8b82-49b9-a9ed-1ed2d7208e81"
+        }
+
+        const expectedResponse:GlobalResponse<any> = {
+            value: null,
+            errors: [
+                "Proveedor de servicios no encontrado."
+            ],
+            success: false
+        }
+        
+        const response = await request(app.app).post("/favorite").set("Authorization", API_TOKEN_DEV).send(body);
+
+        expect(response.body).toEqual(expectedResponse);
+        expect(response.statusCode).toBe(404);
+    });
+
+});
+
+describe("DELETE /favorites", () => {
+
+    test("GOOD", async () => {
+
+        const body = {
+            service_provider_id: "1dde026b-8b82-49b9-a9ed-1ed2d7208e86"
+        }
+        // ADD FAVORITE
+        await request(app.app).post("/favorite").set("Authorization", API_TOKEN_DEV).send(body);
+
+        const expectedResponse:GlobalResponse<any> = {
+            value: null,
+            errors: [],
+            success: true
+        }
+        
+        // REMOVE FAVORITE
+        const response = await request(app.app).delete("/favorite").set("Authorization", API_TOKEN_DEV).send(body);
+
+        expect(response.body).toEqual(expectedResponse);
+        expect(response.statusCode).toBe(200);
+    });
+
+    test("NOT FOUND", async () => {
+
+        const body = {
+            service_provider_id: "1dde026b-8b82-49b9-a9ed-1ed2d7208e81"
+        }
+
+        const expectedResponse:GlobalResponse<any> = {
+            value: null,
+            errors: [
+                "Proveedor de servicios no encontrado."
+            ],
+            success: false
+        }
+        
+        const response = await request(app.app).post("/favorite").set("Authorization", API_TOKEN_DEV).send(body);
+
+        expect(response.body).toEqual(expectedResponse);
+        expect(response.statusCode).toBe(404);
     });
 
 });

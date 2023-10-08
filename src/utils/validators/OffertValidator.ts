9de1ba737b7b31f7f0226633cda78edd82185eca
Diego Cardenas <diegocardwr@gmail.com>
2023-10-07 23:00:09 -0500

    chore: add location validator

--- src/utils/validators/OffertValidator.ts
+++ src/utils/validators/OffertValidator.ts
@@ -1,3 +1,4 @@
+import { LatLng } from 'interfaces/LatLng';
 import { Offert } from '../../interfaces/Offert';
 import joi from 'joi';
 
@@ -21,8 +22,28 @@ export const OffertPostValidator = joi.object<Offert>({
             }),
 });
 
-
 export const OffertPutValidator = joi.object<Offert>({
     price: joi.number(),
     agended_date: joi.date()
+});
+
+export const LocationValidator = joi.object<LatLng>({
+    lat: joi.number()
+            .required()
+            .min(-90)
+            .max(90)
+            .messages({
+                "any.required": "La latitud es requerida.",
+                "number.min": "La latitud minima es -90",
+                "number.max": "La latitud maxima es 90"
+            }),
+    lng: joi.number()
+            .required()
+            .min(-180)
+            .max(180)
+            .messages({
+                "any.required": "La longitud es requerida.",
+                "number.min": "La longitud minima es -180",
+                "number.max": "La longitud maxima es 180"
+            }),
 });
